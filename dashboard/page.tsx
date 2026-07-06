be6f170797b477010776fe6de771f7172566a807
corvexaTemple <sabrililrenhan@gmail.com>
2026-07-06 08:24:53 +0700

    Create page.tsx

---
+++ dashboard/page.tsx
@@ -0,0 +1,20 @@
+import GovernanceCard from "@/components/GovernanceCard";
+import ValidatorCard from "@/components/ValidatorCard";
+
+export default function Dashboard() {
+
+  return (
+
+    <main>
+
+      <h1>Corvexa Dashboard</h1>
+
+      <GovernanceCard />
+
+      <ValidatorCard />
+
+    </main>
+
+  );
+
+}
